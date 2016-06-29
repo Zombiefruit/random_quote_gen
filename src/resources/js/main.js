d527c5d57798a7e6dd381eb0580e1862bcd55384
Kieran Williams <kieranwilliams@MacBook-Pro.local>
2016-06-29 21:13:29 +0300

    pretty much finished

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -1,62 +1,109 @@
-// TODO: load a bunch (like 10) quotes at the beginning and then cycle through them so there is
-// less delay every time the button is pressed to load a new quote. Could also use this to
-// discriminate those quotes whose size is too large to display correctly. The network hit
-// for loading this data is pretty small.
+var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var colourUrl = "http://www.colourlovers.com/api/colors/random";
+var quoteArray = [];
+var flag = 0;
 
-var quoteButton = function() {
-  var xmlObject;
-  var colourObject;
-
-  if (window.XMLHttpRequest) {
-      xmlObject = new XMLHttpRequest();
-      colourObject = new XMLHttpRequest();
-      } else {
-      // code for IE6, IE5
-      xmlObject = new ActiveXObject("Microsoft.XMLHTTP");
-      colourObject = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  xmlObject.onreadystatechange = function() {
-    if (xmlObject.readyState == 4 && xmlObject.status == 200) {
-      var response = xmlObject.responseText;
-      var object = JSON.parse(response);
-
-      document.getElementById("quote").innerHTML = object[0].content;
-      document.getElementById("quote").innerHTML += "<p id=\"author\">" + object[0].title + "</p>";
-    }
-  };
-
-  colourObject.onreadystatechange = function() {
-    if (colourObject.readyState == 4 && colourObject.status == 200) {
-      var response = colourObject.responseText;
-      // var object = JSON.parse(response);
-      var xmlDoc = new DOMParser().parseFromString(response,'text/html');
-
-      hexColour = "#" + xmlDoc.getElementsByTagName("hex")[0].innerText;
-      setColour(hexColour);
-      // var red = xmlDoc.getElementsByTagName("red")[0].innerText;
-      // var green = xmlDoc.getElementsByTagName("green")[0].innerText;
-      // var blue = xmlDoc.getElementsByTagName("blue")[0].innerText;
-    }
-  };
-
-  // TODO: Do you really need two seperate XMLHttpRequest objects for this?
-  // I think yes, as with one you wouldn't be able to check both as getting 200 status
-  //
-  // The line " + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime()" bypasses the cache as described
-  // in the Mozilla XMLHttpRequest docs
-  var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-  var colourUrl = "http://www.colourlovers.com/api/colors/random";
-
-  xmlObject.open("GET", quoteUrl + ((/\?/).test(quoteUrl) ? "&" : "?") + (new Date()).getTime(), true);
-  xmlObject.send();
-
-  colourObject.open("GET", colourUrl + ((/\?/).test(colourUrl) ? "&" : "?") + (new Date()).getTime(), true);
-  colourObject.send();
-
+// Onload do this...
+window.onload = function() {
+  loadQuoteData();
+  loadColourData();
 };
 
+// Set behaviour for when the quote button is clicked
+document.getElementById("refresh").addEventListener("click", function() {
+  displayQuote();
+});
+
+// Display the quote by grabbing the pre-loaded quote from the quote array if it exists.
+// This array acts as a queue data structure. If the next quote is undefined, then set
+// the quote area to display "loading...", and call loadQuoteData(), which will grab a new
+// quote and update the display.
+function displayQuote() {
+  var currentQuote = quoteArray.shift();
+
+  if (currentQuote !== undefined) {
+    document.getElementById("quote").innerHTML = currentQuote[0].content;
+    document.getElementById("quote").innerHTML += "<p id=\"author\">" + currentQuote[0].title + "</p>";
+    var tweetString = currentQuote[0].content.replace(/<(?:.|\n)*?>/gm, '') + " - " + currentQuote[0].title;
+    updateTweet(tweetString);
+    // Only want to set new colour upon changing quote.
+    loadColourData();
+  } else {
+    document.getElementById("quote").innerHTML = "Quote Loading...";
+  }
+
+  loadQuoteData();
+};
+
+// Set the background colour
 function setColour(hexColour) {
-  console.log(hexColour);
   document.getElementById('main').style.background = hexColour;
+};
+
+// Make a call to get a new quote
+function loadQuoteData() {
+  var quoteXmlObject;
+
+  if (window.XMLHttpRequest) {
+      quoteXmlObject = new XMLHttpRequest();
+      } else {
+      // code for IE6, IE5
+      quoteXmlObject = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  quoteXmlObject.onreadystatechange = function() {
+    if (quoteXmlObject.readyState == 4 && quoteXmlObject.status == 200) {
+      var response = quoteXmlObject.responseText;
+      var object = JSON.parse(response);
+
+      // workaround for displaying proper quote length, since the api doesn't offer
+      // length options for free version. This is innefficient, and it is impossible
+      // to predict how many calls to the quote API will be made.
+      if (object[0].content.length < 150) {
+        quoteArray.push(object);
+      } else {
+        loadQuoteData();
+      }
+
+      if (flag === 0 || document.getElementById("quote").innerHTML === "Quote Loading...")  {
+        flag = 1;
+        displayQuote();
+      }
+    }
+  };
+
+  quoteXmlObject.open("GET", quoteUrl + ((/\?/).test(quoteUrl) ? "&" : "?") + (new Date()).getTime(), true);
+  quoteXmlObject.send();
+};
+
+// Load a new colour value. Unfortunately the API has a lot of unavoidable overhead,
+// when all we really want is a simple hex value.
+function loadColourData() {
+    var colourXmlObject;
+
+    if (window.XMLHttpRequest) {
+        colourXmlObject = new XMLHttpRequest();
+        } else {
+        // code for IE6, IE5
+        colourXmlObject = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    colourXmlObject.onreadystatechange = function() {
+      if (colourXmlObject.readyState == 4 && colourXmlObject.status == 200) {
+        var response = colourXmlObject.responseText;
+        var xmlDoc = new DOMParser().parseFromString(response,'text/html');
+
+        hexColour = "#" + xmlDoc.getElementsByTagName("hex")[0].innerText;
+        setColour(hexColour);
+      }
+    };
+
+    colourXmlObject.open("GET", colourUrl + ((/\?/).test(colourUrl) ? "&" : "?") + (new Date()).getTime(), true);
+    colourXmlObject.send();
+};
+
+// Append the tweet anchor tag href with the supplied text
+function updateTweet(string) {
+  var tweetButton = document.getElementById('tweet');
+  tweetButton.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(string));
 };
